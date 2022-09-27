ef347bc6e02280b2254d4146d3bff36d11c2fccd
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-27 23:17:44 +0200

    Display doctors

--- src/components/DoctorList.js
+++ src/components/DoctorList.js
@@ -5,10 +5,17 @@ const DoctorList = ({name, email, location, avatar }) => {
   return (
     <>
     <section className="doctor-list">
-        <p>{name}</p>
-        <p>{email}</p>
-        <p>{location}</p>
+      <div className="doc-div">
+        <div className="avatar-div">
         <img src={avatar} alt="avatar"/>
+        </div>
+        <div className="doc-infor">
+          <h2 className="doc-name">{name}</h2>
+          <span className="doc-email">{email}</span>
+          <span className="doc-location">{location}</span>
+        </div>
+        <a href="#" className="View-btn">View Doctor</a> 
+      </div>
     </section>
     </>
   )
@@ -18,7 +25,6 @@ DoctorList.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  speciality: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
 };
 
