527d618b07e7988260e80ad6823f9f6cf6c6d66f
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-28 23:00:29 +0100

    Add plugins to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -103,4 +103,9 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
 };
