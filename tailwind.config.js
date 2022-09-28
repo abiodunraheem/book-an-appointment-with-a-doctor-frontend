acf7a9eb488d36ae089c6f7b5210019c1cb01e26
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-28 22:48:24 +0100

    Fix linters error

--- tailwind.config.js
+++ tailwind.config.js
@@ -95,7 +95,6 @@ module.exports = {
           none: '0',
           sm: '0.125rem',
           DEFAULT: '0.25rem',
-          DEFAULT: '4px',
           md: '0.375rem',
           lg: '0.5rem',
           full: '9999px',
@@ -103,10 +102,5 @@ module.exports = {
         },
       },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
+  }
 };
