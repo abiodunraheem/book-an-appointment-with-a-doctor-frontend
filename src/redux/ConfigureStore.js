4261f9872cae2a4fc726940d00af29df723384af
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-28 09:40:54 +0100

    Add doctorReducers to configure store

---
+++ src/redux/ConfigureStore.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import doctorReducer from './doctors/doctor';
+import doctorFetchReducer from './doctors/DoctorList';
+import doctorDeleteReducer from './doctors/doctorDelete';
+
+const reducer = combineReducers({
+  MostRecent: doctorFetchReducer,
+  doctor: doctorReducer,
+  deleteDoctor: doctorDeleteReducer,
+});
+
+const store = configureStore({ reducer }, applyMiddleware(thunk, logger));
+
+export default store;
