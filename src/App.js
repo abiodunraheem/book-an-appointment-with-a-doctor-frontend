a3bdb0471782569a4e9663e8dc1109ad2b5463ed
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-29 14:19:04 +0100

    Use addDoctor component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
+import AddDoctor from './components/AddDoctor';
 
 function App() {
   return (
     <div className="App">
-
+      <AddDoctor />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
