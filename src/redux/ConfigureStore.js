d82a78e491c350b0f05e12b6d12b41b9ac67cb9e
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-29 14:16:04 +0100

    Add addDoctorReducer to store

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import doctorReducer from './doctor/DoctorListReducer';
+import addDoctorReducer from './doctor/AddDoctorReducer';
 
 const store = configureStore({
   reducer: {
     doctors: doctorReducer,
+    doctor: addDoctorReducer,
   },
 });
 
