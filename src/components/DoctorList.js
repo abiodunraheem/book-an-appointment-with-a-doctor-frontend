39759a71e3589066f4dd522162a6ff64eb23ff21
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-29 11:33:12 +0100

    Create doctorlist component

--- src/components/DoctorList.js
+++ src/components/DoctorList.js
@@ -13,67 +13,7 @@ const DoctorList = ({ name, email, location }) => (
   <>
     <section className="doctor-list">
 
-      <Card sx={{
-        minWidth: 239,
-        height: 'auto',
-        marginTop: 5,
-        paddingTop: 1,
-        boxShadow: 0,
-
-      }}
-      >
-        <CardMedia
-          component="img"
-          image={avatar}
-          alt="{name}"
-          sx={materialImage}
-        />
-        <CardContent
-          sx={{
-            width: 200,
-            border: 'none',
-          }}
-        >
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={materialName}
-          >
-            {name}
-          </Typography>
-          <div style={dotStyle}>.......................</div>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              textAlign: 'center',
-              marginLeft: '-15px',
-            }}
-          >
-            {email}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              textAlign: 'center',
-              marginLeft: '-15px',
-            }}
-          >
-            {location}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link
-            href="#dctors"
-            underline="none"
-            sx={materialName.view}
-          >
-            View Doctor
-          </Link>
-        </CardActions>
-      </Card>
+      
     </section>
   </>
 );
