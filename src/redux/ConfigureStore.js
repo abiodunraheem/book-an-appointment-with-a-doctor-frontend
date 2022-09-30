c0db427d5b5bcb986277a88d2bc46865372b100f
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-30 12:19:52 +0200

    Add the register reducer in the store

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import doctorReducer from './doctor/DoctorListReducer';
+import register from './user/RegisterLoginSlice';
 
 const store = configureStore({
   reducer: {
     doctors: doctorReducer,
+    user : register,
   },
 });
 
