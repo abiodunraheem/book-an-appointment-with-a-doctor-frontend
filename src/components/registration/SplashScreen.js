c70fbeb388b1d483ca07fd245aca7ed14e697481
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-03 09:25:52 +0200

    Add the backgroung images

--- src/components/registration/SplashScreen.js
+++ src/components/registration/SplashScreen.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../../assets/images/doc.png';
 
 const SplashScreen = () => (
   <>
     <section className="splash-sec">
       <div className="top">
         <div className="logo-div">
-          <img src="" alt />
+          <img src={logo} alt="name" />
         </div>
         <div className="welcome-text">
           <h1 className="text">Welcome Back</h1>
           <p className="text-two">Login to your Account</p>
         </div>
         <div className="login-btn">
-        <NavLink to="/login">
-        <span>Login</span>
-      </NavLink>
+          <NavLink to="/login">
+            <span>Login</span>
+          </NavLink>
         </div>
       </div>
+      <div className="bottom" />
 
     </section>
   </>
