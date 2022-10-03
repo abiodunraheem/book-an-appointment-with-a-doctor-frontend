539823d8d256b1db759c5dd13719b24faf12c9e9
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-03 09:00:57 +0200

    Add the login link to the splash

--- src/components/registration/SplashScreen.js
+++ src/components/registration/SplashScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SplashScreen = () => (
   <>
-    <h1>SplashScreen</h1>
+    <section className="splash-sec">
+      <div className="top">
+        <div className="logo-div">
+          <img src="" alt />
+        </div>
+        <div className="welcome-text">
+          <h1 className="text">Welcome Back</h1>
+          <p className="text-two">Login to your Account</p>
+        </div>
+        <div className="login-btn">
+        <NavLink to="/login">
+        <span>Login</span>
+      </NavLink>
+        </div>
+      </div>
+
+    </section>
   </>
 );
 
