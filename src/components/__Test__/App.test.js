6de6d61b21b34f5cc568af476d6a8894a63b29b4
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-04 08:50:50 +0200

    Add the tests for the App component

--- src/components/__Test__/App.test.js
+++ src/components/__Test__/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../../../redux/store';
-import App from '../../../App';
+import store from '../../redux/ConfigureStore';
+import App from '../../App';
+
 
 describe('App', () => {
   it('renders correctly', () => {
