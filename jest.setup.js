f8673c26464864e4c26f140ecc307418a76f0426
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-10-05 23:31:45 +0100

    remove some files

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
