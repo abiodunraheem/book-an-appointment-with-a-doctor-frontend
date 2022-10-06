28b991447291762857ae5ff8de52ee6d45639138
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-10-06 01:32:34 +0100

    Add change

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
+import App from '../App';
 import store from '../redux/ConfigureStore';
 import Reservations from '../components/Reservations';
 import Reserve from '../components/Reserve';
@@ -8,6 +9,10 @@ import Reserve from '../components/Reserve';
 const createComponent = (component) => <Provider store={store}>{component}</Provider>;
 
 describe('Test for components', () => {
+  test('renders correctly', async () => {
+    renderer.create(<App />);
+  });
+
   test('Reservation page should render correctly', () => {
     const component = renderer
       .create(createComponent(<Reservations />))
--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { addReservations } from '../redux/reserve/ReservationFormReducer';
 import { fetchDoctors } from '../redux/doctor/DoctorListReducer';
@@ -15,13 +14,15 @@ function Reserve() {
 
   const doctor = useSelector((state) => state.doctors.doctor);
   const doctors = Array.from(doctor);
-  const navigate = useNavigate();
 
+  // const path = window.location.pathname;
+  const { register, handleSubmit } = useForm();
+
+  // onSubmit go to the path /reservations
   const onSubmit = (doctors) => {
     dispatch(addReservations(doctors));
-    navigate('/reservations');
+    window.location.href = '/reservations';
   };
-  const { register, handleSubmit } = useForm();
 
   return (
     <div className="container-fluid flex w-full">
