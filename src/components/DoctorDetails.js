d01f1353678ba479d41a30546045acaba0f04253
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-07 11:05:28 +0100

    Refactor code to include doctor details

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -17,7 +17,34 @@ const DoctorDetails = () => {
           <div className="avatar-container">
             <img src={item.avatar} alt={item.name} />
           </div>
-          
+          <div className="contents p-5">
+            <div className="p-3">
+              <p>Name: </p>
+              <h3>{item.name}</h3>
+            </div>
+
+            <div className="p-3 my-2">
+              <p>Speciality: </p>
+              {' '}
+              <h4>{item.speciality}</h4>
+            </div>
+            <div className="p-3 my-2">
+              <p>Bill: $</p>
+              {' '}
+              <h4>{item.bill}</h4>
+            </div>
+            <div className="p-3 my-2">
+              <p>Location: </p>
+              {' '}
+              <h4>{item.location}</h4>
+            </div>
+            <div className="p-3 my-2">
+              <p>Email: </p>
+              {' '}
+              <h4>{item.email}</h4>
+            </div>
+            <Link className="btn reserve-btn my-2" to={`/reservationForm/${item.id}`}>Reserve Now</Link>
+          </div>
         </div>
       ))}
     </div>
