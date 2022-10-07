9bd708bb2af53925792371ec30c2550458e36509
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-07 20:54:13 +0200

    Add and update the details component

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,159 +1,55 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { Link, useParams } from 'react-router-dom';
-// import { fetchDoctors } from '../redux/doctor/DoctorListReducer';
-// import '../App.css';
-
-// const DoctorDetails = () => {
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(fetchDoctors());
-//   }, [dispatch]);
-//   const doctor = useParams();
-//   const doctorList = useSelector((state) => state.doctors.doctor);
-//   const doctors = Array.from(doctorList);
-//   const doctorDetails = doctors.filter((item) => item.id === doctor.id);
-//   console.log(doctorDetails);
-
-//   return (
-//     <div className="container my-5
-//      "
-//     >
-//       {doctorDetails.doctor?.map((item) => (
-//         <div className="details-container" key={item.id}>
-//           <div className="avatar-container">
-//             <img src={item.avatar} alt={item.name} />
-//           </div>
-//           <div className="contents p-5">
-//             <div className="p-3">
-//               <p>Name: </p>
-//               <h3>{item.name}</h3>
-//             </div>
-
-//             <div className="p-3 my-2">
-//               <p>Speciality: </p>
-//               {' '}
-//               <h4>{item.speciality}</h4>
-//             </div>
-//             <div className="p-3 my-2">
-//               <p>Bill: $</p>
-//               {' '}
-//               <h4>{item.bill}</h4>
-//             </div>
-//             <div className="p-3 my-2">
-//               <p>Location: </p>
-//               {' '}
-//               <h4>{item.location}</h4>
-//             </div>
-//             <div className="p-3 my-2">
-//               <p>Email: </p>
-//               {' '}
-//               <h4>{item.email}</h4>
-//             </div>
-//             <Link className="btn reserve-btn my-2" to={`/reserve/${item.id}`}>Reserve Now</Link>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default DoctorDetails;
-// import React, { useEffect, useState } from 'react';
-// import { NavLink, useParams } from 'react-router-dom';
-// import axios from 'axios';
-
-// const DoctorDetails = () => {
-//   const { id } = useParams();
-//   const [doctor, setDoctor] = useState({});
-//   useEffect(() => {
-//     const res = axios
-//       .get(`http://127.0.0.1:3000/api/v1/doctors/${id}`)
-//       .then((response) => setDoctor());
-//     console.log(res);
-//   }, []);
-//   return (
-//     <div className="container my-5
-//      "
-//     >
-//       <div className="contents p-5">
-//         <div className="p-3">
-//           <p>Name: </p>
-//           <h3>{doctor.name}</h3>
-//         </div>
-//         <div className="p-3 my-2">
-//           <p>Speciality: </p>
-//           {' '}
-//           <h4>{doctor.speciality}</h4>
-//         </div>
-//         <div className="p-3 my-2">
-//           <p>Bill: $</p>
-//           {' '}
-//           <h4>{doctor.bill}</h4>
-//         </div>
-//         <div className="p-3 my-2">
-//           <p>Location: </p>
-//           {' '}
-//           <h4>{doctor.location}</h4>
-//         </div>
-//         <div className="p-3 my-2">
-//           <p>Email: </p>
-//           {' '}
-//           <h4>{doctor.email}</h4>
-//         </div>
-//         <NavLink className="btn reserve-btn my-2" to={`/reservationForm/${doctor.id}`}>Reserve doctor</NavLink>
-//       </div>
-//     </div>
-//   );
-// };
-// export default DoctorDetails;
-
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+// import { useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
-import { fetchDoctors } from '../redux/doctor/DoctorListReducer';
+// import { fetchDoctors } from '../redux/doctor/DoctorListReducer';
 
-function DoctorDetails() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchDoctors());
-  }, [dispatch]);
-  const doctorList = useSelector((state) => state.doctors.doctor);
-  const doctors = Array.from(doctorList);
-
+const DoctorDetails = () => {
+  // const doctorList = useSelector((state) => state.doctors.doctor);
+  // const doctors = Array.from(doctorList);
   const params = useParams();
-  const doctor = doctors.find((doctor) => doctor.id === Number(params.id));
+  // const doctor = doctors.filter((doctor) => doctor.id === Number(params.id));
+  // const selectedDoctor = doctor[0];
+
+  // const user = useSelector((state) => state.user);
+  // console.log(user);
+
+  // to get Active logged in current user's id
+
+  const currentDoctors = localStorage.getItem('store') || '';
+  const json = JSON.parse(currentDoctors);
+  const { doctor } = json.doctors;
+  const filterdDoctor = doctor.filter((doctor) => doctor.id === Number(params.id));
+  console.log(filterdDoctor);
   console.log(params);
-  console.log(doctor);
-  const {
-    name, speciality, email, image, bill,
-  } = doctor;
 
   return (
     <div id="RouterNavLink" className="doctor-container">
       <div className="image-container">
-        <img src={image} alt="Profile of doctor" className="single-image" />
+        <img src={filterdDoctor[0].avatar} alt="Profile of doctor" className="single-image" />
       </div>
       <div className="details">
-        <h4 className="detail-name">{name}</h4>
+        <h4 className="detail-name">{filterdDoctor[0].name}</h4>
         <table>
           <tr className="col">
             <th>Speciality</th>
-            <td>{speciality}</td>
+            <td>{filterdDoctor[0].speciality}</td>
           </tr>
           <tr>
             <th>Email</th>
-            <td>{email}</td>
+            <td>{filterdDoctor[0].email}</td>
           </tr>
           <tr className="col">
             <th>Bill</th>
-            <td>{bill}</td>
+            <td>{filterdDoctor[0].bill}</td>
           </tr>
         </table>
         <Link to="/new_appointment" className="btn-details btn mt-4 text-center">Book Appointment</Link>
       </div>
     </div>
   );
-}
+};
+// DoctorDetails.propTypes = {
+//   doctors: PropTypes.array.isRequired,
+// };
 
 export default DoctorDetails;
