077bf6ada8b8df0b6d4ee912f871f659ae3fc9aa
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-07 16:53:03 +0200

    Modify the doctor details

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -12,8 +12,8 @@
 //   const doctor = useParams();
 //   const doctorList = useSelector((state) => state.doctors.doctor);
 //   const doctors = Array.from(doctorList);
-//   const doctorDetails = doctors.filter((item) => +item.id === +doctor.id);
-//   console.log(doctors);
+//   const doctorDetails = doctors.filter((item) => item.id === doctor.id);
+//   console.log(doctorDetails);
 
 //   return (
 //     <div className="container my-5
@@ -59,50 +59,101 @@
 // };
 
 // export default DoctorDetails;
-import React, { useEffect, useState } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import { NavLink, useParams } from 'react-router-dom';
+// import axios from 'axios';
 
-const DoctorDetails = () => {
-  const { id } = useParams();
-  const [doctor, setDoctor] = useState({});
+// const DoctorDetails = () => {
+//   const { id } = useParams();
+//   const [doctor, setDoctor] = useState({});
+//   useEffect(() => {
+//     const res = axios
+//       .get(`http://127.0.0.1:3000/api/v1/doctors/${id}`)
+//       .then((response) => setDoctor());
+//     console.log(res);
+//   }, []);
+//   return (
+//     <div className="container my-5
+//      "
+//     >
+//       <div className="contents p-5">
+//         <div className="p-3">
+//           <p>Name: </p>
+//           <h3>{doctor.name}</h3>
+//         </div>
+//         <div className="p-3 my-2">
+//           <p>Speciality: </p>
+//           {' '}
+//           <h4>{doctor.speciality}</h4>
+//         </div>
+//         <div className="p-3 my-2">
+//           <p>Bill: $</p>
+//           {' '}
+//           <h4>{doctor.bill}</h4>
+//         </div>
+//         <div className="p-3 my-2">
+//           <p>Location: </p>
+//           {' '}
+//           <h4>{doctor.location}</h4>
+//         </div>
+//         <div className="p-3 my-2">
+//           <p>Email: </p>
+//           {' '}
+//           <h4>{doctor.email}</h4>
+//         </div>
+//         <NavLink className="btn reserve-btn my-2" to={`/reservationForm/${doctor.id}`}>Reserve doctor</NavLink>
+//       </div>
+//     </div>
+//   );
+// };
+// export default DoctorDetails;
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
+import { fetchDoctors } from '../redux/doctor/DoctorListReducer';
+
+function DoctorDetails() {
+  const dispatch = useDispatch();
   useEffect(() => {
-    axios
-      .get(`http://127.0.0.1:3000/api/v1/doctors/${id}`)
-      .then((response) => setDoctor(response.data.doctor));
-  }, []);
+    dispatch(fetchDoctors());
+  }, [dispatch]);
+  const doctorList = useSelector((state) => state.doctors.doctor);
+  const doctors = Array.from(doctorList);
+
+  const params = useParams();
+  const doctor = doctors.find((doctor) => doctor.id === Number(params.id));
+  console.log(params);
+  console.log(doctor);
+  const {
+    name, speciality, email, image, bill,
+  } = doctor;
+
   return (
-    <div className="container my-5
-     "
-    >
-      <div className="contents p-5">
-        <div className="p-3">
-          <p>Name: </p>
-          <h3>{doctor.name}</h3>
-        </div>
-        <div className="p-3 my-2">
-          <p>Speciality: </p>
-          {' '}
-          <h4>{doctor.speciality}</h4>
-        </div>
-        <div className="p-3 my-2">
-          <p>Bill: $</p>
-          {' '}
-          <h4>{doctor.bill}</h4>
-        </div>
-        <div className="p-3 my-2">
-          <p>Location: </p>
-          {' '}
-          <h4>{doctor.location}</h4>
-        </div>
-        <div className="p-3 my-2">
-          <p>Email: </p>
-          {' '}
-          <h4>{doctor.email}</h4>
-        </div>
-        <NavLink className="btn reserve-btn my-2" to={`/reservationForm/${doctor.id}`}>Reserve doctor</NavLink>
+    <div id="RouterNavLink" className="doctor-container">
+      <div className="image-container">
+        <img src={image} alt="Profile of doctor" className="single-image" />
+      </div>
+      <div className="details">
+        <h4 className="detail-name">{name}</h4>
+        <table>
+          <tr className="col">
+            <th>Speciality</th>
+            <td>{speciality}</td>
+          </tr>
+          <tr>
+            <th>Email</th>
+            <td>{email}</td>
+          </tr>
+          <tr className="col">
+            <th>Bill</th>
+            <td>{bill}</td>
+          </tr>
+        </table>
+        <Link to="/new_appointment" className="btn-details btn mt-4 text-center">Book Appointment</Link>
       </div>
     </div>
   );
-};
+}
+
 export default DoctorDetails;
--- src/components/DoctorList.js
+++ src/components/DoctorList.js
@@ -1,79 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import avatar from '../assets/images/doctor1.jpg';
 import { dotStyle, materialImage, materialName } from './Styles';
 
-const DoctorList = ({ name, email, location }) => (
-  <>
-    <section className="mt-10">
-      <Card sx={{
-        minWidth: 208,
-        height: 'auto',
-        paddingTop: 2,
-        boxShadow: 0,
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-      >
-        <CardMedia
-          className="border-2 border-blueSecond rounded-full"
-          component="img"
-          image={avatar}
-          alt="{name}"
-          sx={materialImage}
-        />
-        <CardContent
-          sx={{
-            border: 'none',
-          }}
+function DoctorList({ name, email, location }) {
+  return (
+    <>
+      <section className="mt-10">
+        <Card sx={{
+          minWidth: 208,
+          height: 'auto',
+          paddingTop: 2,
+          boxShadow: 0,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
         >
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={materialName}
-          >
-            {name}
-          </Typography>
-          <div style={dotStyle}>.......................</div>
-          <Typography
-            variant="body2"
-            color="text.secondary"
+          <CardMedia
+            className="border-2 border-blueSecond rounded-full"
+            component="img"
+            image={avatar}
+            alt="{name}"
+            sx={materialImage}
+          />
+          <CardContent
             sx={{
-              textAlign: 'center',
+              border: 'none',
             }}
           >
-            {email}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              textAlign: 'center',
-            }}
-          >
-            {location}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link
-            href="/user/doctors"
-            underline="none"
-            sx={materialName.view}
-          >
-            View Doctor
-          </Link>
-        </CardActions>
-      </Card>
-    </section>
-  </>
-);
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={materialName}
+            >
+              {name}
+            </Typography>
+            <div style={dotStyle}>.......................</div>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {email}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {location}
+            </Typography>
+          </CardContent>
+        </Card>
+      </section>
+    </>
+  );
+}
 
 DoctorList.propTypes = {
   name: PropTypes.string.isRequired,
