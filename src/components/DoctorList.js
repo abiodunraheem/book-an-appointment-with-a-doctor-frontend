9f251eac72f43fdf6e545fbddff6114b62938998
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-07 10:55:01 +0100

    Add card actions to display doctors

--- src/components/DoctorList.js
+++ src/components/DoctorList.js
@@ -61,15 +61,7 @@ const DoctorList = ({ name, email, location }) => (
             {location}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Link
-            href="#doctors"
-            underline="none"
-            sx={materialName.view}
-          >
-            View Doctor
-          </Link>
-        </CardActions>
+        
       </Card>
     </section>
   </>
