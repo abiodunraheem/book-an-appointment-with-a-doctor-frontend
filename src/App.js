00e01848fbe29562f9285e2d09b23a9539ac6aab
medsonmoombe <medsonmoombe21@gmail.com>
2022-10-07 20:54:13 +0200

    Add and update the details component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Home from './components/Home';
 import Reservations from './components/Reservations';
 import Reserve from './components/Reserve';
@@ -8,9 +10,15 @@ import SplashScreen from './components/registration/SplashScreen';
 import Login from './components/registration/Login';
 import DoctorDetails from './components/DoctorDetails';
 import DoctorList from './components/DoctorList';
+import { fetchDoctors } from './redux/doctor/DoctorListReducer';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDoctors());
+  }, [dispatch]);
+  const doctors = useSelector((state) => state.doctors.doctor);
   const SidebarLayout = () => (
     <>
       <SideBar />
@@ -28,7 +36,7 @@ function App() {
           <Route path="/reservations" element={<Reservations />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/user/doctors/" element={<DoctorList />} />
-          <Route path="/user/doctors/:id" element={<DoctorDetails />} />
+          <Route path="/user/doctors/:id" element={<DoctorDetails doctors={doctors} />} />
         </Route>
       </Routes>
     </div>
